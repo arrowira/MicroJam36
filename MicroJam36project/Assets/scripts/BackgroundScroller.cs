7f6027e5563dfe7480811d58d657522db40d65f2
arrowira <41136800+arrowira@users.noreply.github.com>
2025-03-21 22:14:06 -0700

    background texture

--- MicroJam36project/Assets/scripts/BackgroundScroller.cs
+++ MicroJam36project/Assets/scripts/BackgroundScroller.cs
@@ -7,6 +7,7 @@ public class BackgroundScroller : MonoBehaviour
     float speed = 0.077f;
     public GameObject B1;
     public GameObject B2;
+    public GameObject B3;
     void Start()
     {
         
@@ -15,13 +16,18 @@ public class BackgroundScroller : MonoBehaviour
     {
         B1.transform.position = new Vector3(B1.transform.position.x - speed, B1.transform.position.y, 100);
         B2.transform.position = new Vector3(B2.transform.position.x - speed, B2.transform.position.y, 100);
-        if(B1.transform.position.x <= -15)
+        B3.transform.position = new Vector3(B2.transform.position.x - speed, B2.transform.position.y, 100);
+        if (B1.transform.position.x <= -15)
         {
             B1.transform.position = new Vector3(15, B1.transform.position.y, 100);
         }
         if (B2.transform.position.x <= -15)
         {
             B2.transform.position = new Vector3(15, B2.transform.position.y, 100);
+        }
+        if (B3.transform.position.x <= -15)
+        {
+            B3.transform.position = new Vector3(15, B3.transform.position.y, 100);
         }
     }
 }
